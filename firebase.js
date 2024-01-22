6e6fd4de37090a7691aa38db9923fd6a51867862
Dnyanesh Bachhav <dnyaneshbachhav2002gmail.com>
2024-01-23 00:46:23 +0530

    Firebase email integration...

--- firebase.js
+++ firebase.js
@@ -6,15 +6,13 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-s
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  //add your config here
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyDKCYfWGO-E68MB027duUvNDzFcigN2vHc",
+  authDomain: "dsbda-project-website.firebaseapp.com",
+  projectId: "dsbda-project-website",
+  storageBucket: "dsbda-project-website.appspot.com",
+  messagingSenderId: "1070580859502",
+  appId: "1:1070580859502:web:026c8368654c5474bbdc7f",
+  measurementId: "G-44WZ3NC3S8"
 };
 
 // Initialize Firebase
